d8db28a13fe6e578817252cb4ca7afa8fda88790
Gearz <shanegeary@yahoo.com>
2021-02-06 07:45:37 -0700

    tickets create and strong params methods and route

--- app/controllers/api/v1/tickets_controller.rb
+++ app/controllers/api/v1/tickets_controller.rb
@@ -1,2 +1,20 @@
 class Api::V1::TicketsController < ApplicationController
+    def index
+        tickets = Ticket.all
+        render json: tickets 
+    end
+
+    def create 
+        ticket = Ticket.new(ticket_params)
+        if ticket.save 
+            render json: ticket, status: :accepted   
+        else
+            render json: {errors: ticket.errors.full_messages}, status: :unprocessible_entity
+        end
+    end
+
+    private 
+    def ticket_params
+        params.require(:ticket).permit(:title, :date, :main_act, :category_id)
+    end
 end
